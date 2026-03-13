cbd0d75d80f804c24ba1964eb173c9b153a0cb7a
rapid <rapidjonte@gmail.com>
2026-03-13 14:34:10 +0100

    i forgor

--- scripts/block.gd
+++ scripts/block.gd
@@ -8,10 +8,10 @@ func _ready():
 	Global.flip_blocks.connect(flip_self)
 	
 func _process(delta: float) -> void:
-	if character_body.velocity.y * character_body.gravity >= 0:
-		one_way_collision_margin = 24
+	if character_body.gravity * character_body.velocity.y >= -0.0001:
+		set_deferred("one_way_collision_margin", 32)
 	else:
-		one_way_collision_margin = 1
+		set_deferred("one_way_collision_margin", 1)
 
 func flip_self():
 	rotation += deg_to_rad(180)
