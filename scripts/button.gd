97f7088f7835e1d204527a34b57d5f7b89cdc88b
rapid <rapidjonte@gmail.com>
2026-03-19 23:36:31 +0100

    removed old menu

--- scripts/button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-@export var levelPath: String
-
-var game = load("res://scenes/game.tscn")
-
-func _on_pressed() -> void:
-	Global.load_level(levelPath)
-	get_tree().change_scene_to_packed(game)
